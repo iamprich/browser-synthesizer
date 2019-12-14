e65b9f7844333205390738595fc429026e264c0e
Paul Richardson <paul.richardsona@gmail.com>
2019-12-13 22:04:05 -0500

    adds lcd screen

---
+++ keyboard.js
@@ -0,0 +1,41 @@
+const PLAYABLE_KEYS = {
+  KeyA: 60,
+  KeyW: 61,
+  KeyS: 62,
+  KeyE: 63,
+  KeyD: 64,
+  KeyF: 65,
+  KeyT: 66,
+  KeyG: 67,
+  KeyY: 68,
+  KeyH: 69,
+  KeyU: 70,
+  KeyJ: 71,
+  KeyK: 72,
+}
+
+export default class Keyboard {
+  static playable_keys = PLAYABLE_KEYS
+  static active_notes = []
+
+  static get playableKeys () {
+    return this.playable_keys
+  }
+
+  static get activeNotes () {
+    return this.active_notes
+  }
+
+  static get currentNote () {
+    return this.active_notes[this.active_notes.length - 1]
+  }
+
+  static set noteOn (keyCode) {
+    if (this.active_notes.includes(keyCode)) return
+    this.active_notes.push(keyCode)
+  }
+
+  static set noteOff (keyCode) {
+    this.active_notes.splice(this.active_notes.indexOf(keyCode), 1)
+  }
+}
--- synthesizer.js
+++ synthesizer.js
@@ -1,181 +1,146 @@
+import Keyboard from './keyboard.js'
+
 // Audio context and master out
-const CONTEXT = new AudioContext();
-const MASTER_OUT = CONTEXT.destination;
+const CONTEXT = new (window.AudioContext || window.webkitAudioContext)()
+const MASTER_OUT = CONTEXT.destination
+
+// LCD
+const LCD = document.getElementById('inner-display')
 
 // VCO
-const VCO = CONTEXT.createOscillator();
-VCO.type = 'sine';
-VCO.start(0);
+const VCO = CONTEXT.createOscillator()
+VCO.type = 'sine'
+VCO.start(0)
 
-let oscillator_selector = document.getElementById('oscillatorSelector');
+let oscillator_selector = document.getElementById('oscillatorSelector')
 oscillator_selector.addEventListener(
-    'change',
-    e => (VCO.type = e.target.value),
-);
+  'change',
+  e => (VCO.type = e.target.value),
+)
 
 // VCA
-const VCA = CONTEXT.createGain();
-VCA.gain.setValueAtTime(0.0001, CONTEXT.currentTime);
-let volume = 0.2;
+const VCA = CONTEXT.createGain()
+VCA.gain.setValueAtTime(0.0001, CONTEXT.currentTime)
+let volume = 0.2
 
-let vca_volume = document.getElementById('vcaVolume');
+let vca_volume = document.getElementById('vcaVolume')
 vca_volume.addEventListener('input', e => {
-  volume = e.target.value;
-  // VCA.gain.linearRampToValueAtTime(volume, CONTEXT.currentTime + 0.02);
-  VCA.gain.setValueAtTime(volume, CONTEXT.currentTime);
-});
+  volume = e.target.value
+  VCA.gain.setValueAtTime(volume, CONTEXT.currentTime)
+})
 
 // Routing
-VCO.connect(VCA);
-VCA.connect(MASTER_OUT);
+VCO.connect(VCA)
+VCA.connect(MASTER_OUT)
 
 // MIDI
 if (navigator.requestMIDIAccess) {
   navigator.requestMIDIAccess({
     sysex: false,
-  }).then(onMIDISuccess, onMIDIFailure);
+  }).then(onMIDISuccess, onMIDIFailure)
 } else {
-  console.log('No MIDI support in your browser.');
+  console.log('No MIDI support in your browser.')
 }
 
-function onMIDISuccess(midiAccess) {
-  let inputs = midiAccess.inputs.values();
+function onMIDISuccess (midiAccess) {
+  let inputs = midiAccess.inputs.values()
   for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
     //  call onMIDIMessage for each midi call
-    input.value.onmidimessage = onMIDIMessage;
+    input.value.onmidimessage = onMIDIMessage
   }
 }
 
-let midi_message_on = 144;
-let midi_message_off = 128;
+let midi_message_on = 144
+let midi_message_off = 128
 
-function onMIDIMessage(event) {
-  let data = event.data;
-  let type = data[0] & 0xf0;
-  let note_number = data[1];
-  let velocity = data[2];
+function onMIDIMessage (event) {
+  let data = event.data
+  let type = data[0] & 0xf0
+  let note_number = data[1]
+  let velocity = data[2]
 
   switch (type) {
     case midi_message_on:
       if (velocity !== 0) {
-        noteOn(note_number);
+        noteOn(note_number)
       }
-      break;
+      break
 
     case midi_message_off:
-      noteOff(note_number);
-      break;
+      noteOff(note_number)
+      break
   }
 }
 
-function onMIDIFailure(e) {
-  console.log(`No access to MIDI devices ${e}`);
+function onMIDIFailure (e) {
+  console.log(`No access to MIDI devices ${e}`)
 }
 
-// Note functions
-let active_notes = {};
-
-function noteOn(keyCode) {
-  let frequency = frequencyFromNoteNumber(playable_keys[keyCode]);
-  VCO.frequency.setValueAtTime(frequency, CONTEXT.currentTime);
-  VCA.gain.linearRampToValueAtTime(volume, CONTEXT.currentTime + 0.02);
-  active_notes[keyCode] = 'active';
-  document.querySelector(`[data-key='${keyCode}']`).classList.add('playing');
+function noteOn (keyCode) {
+  let frequency = frequencyFromNoteNumber(Keyboard.playableKeys[keyCode])
+  VCO.frequency.setValueAtTime(frequency, CONTEXT.currentTime)
+  VCA.gain.linearRampToValueAtTime(volume, CONTEXT.currentTime + 0.02)
+  Keyboard.noteOn = keyCode
+  LCD.innerHTML = keyCode
+  document.querySelector(`[data-key='${keyCode}']`).classList.add('playing')
 }
 
-function noteOff(keyCode) {
-  delete active_notes[keyCode];
-  document.querySelector(`[data-key='${keyCode}']`).classList.remove('playing');
+function noteOff (keyCode) {
+  Keyboard.noteOff = keyCode
+  LCD.innerHTML = ''
+  document.querySelector(`[data-key='${keyCode}']`).classList.remove('playing')
 
-  if (Object.keys(active_notes).length === 0) {
-    VCA.gain.linearRampToValueAtTime(0.0001, CONTEXT.currentTime + 0.02);
-  }
-
-  if (Object.values(active_notes).includes('active')) {
-    let previous_note = Object.keys(active_notes)[
-    Object.keys(active_notes).length - 1
-        ];
-    noteOn(previous_note);
+  if (Keyboard.activeNotes.length === 0) {
+    VCA.gain.linearRampToValueAtTime(0.0001, CONTEXT.currentTime + 0.02)
+  } else {
+    noteOn(Keyboard.currentNote)
   }
 }
 
-function frequencyFromNoteNumber(note) {
-  return 440 * Math.pow(2, (note - 69) / 12);
+function frequencyFromNoteNumber (note) {
+  return 440 * Math.pow(2, (note - 69) / 12)
 }
 
-//  TODO would be cool if the keyboard was all state based. Own class!
-let playable_keys = {
-  KeyA: 60,
-  KeyW: 61,
-  KeyS: 62,
-  KeyE: 63,
-  KeyD: 64,
-  KeyF: 65,
-  KeyT: 66,
-  KeyG: 67,
-  KeyY: 68,
-  KeyH: 69,
-  KeyU: 70,
-  KeyJ: 71,
-  KeyK: 72,
-};
+function changeOctave (direction) {
+  let octave = 12
 
-function changeOctave(direction) {
-  // REFACTOR this into a constant
-  // TODO set a max octave count and display current octave
-  // max +-3
-  let octave = 12;
-
-  Object.keys(playable_keys).forEach(pitch => {
+  Object.keys(Keyboard.playableKeys).forEach(pitch => {
     if (direction === 'up') {
-      playable_keys[pitch] += octave;
+      Keyboard.playableKeys[pitch] += octave
     } else {
-      playable_keys[pitch] -= octave;
+      Keyboard.playableKeys[pitch] -= octave
     }
-
-    //  TODO re-trigger the active note here
-  });
+  })
+  if (Keyboard.currentNote) noteOn(Keyboard.currentNote)
 }
 
-// function octaveHandler(e) {
-//   let direction = e.target.dataset.direction;
-//   direction === 'up' ? changeOctave('up') : changeOctave('down');
-// }
-//
-// octave_up = document.getElementById('octave-up');
-// octave_up.addEventListener('click', octaveHandler);
-//
-// octave_down = document.getElementById('octave-down');
-// octave_down.addEventListener('click', octaveHandler);
+document.addEventListener('keydown', keyHandler)
+document.addEventListener('keyup', keyHandler)
 
-document.addEventListener('keydown', keyHandler);
-document.addEventListener('keyup', keyHandler);
-
-function keyHandler(e) {
-  if (e.repeat) return;
+function keyHandler (e) {
+  if (e.repeat) return
 
   switch (e.type) {
     case 'keydown':
-      if (playable_keys.hasOwnProperty(e.code)) noteOn(e.code);
-      if (e.code === 'KeyX') changeOctave('up');
-      if (e.code === 'KeyZ') changeOctave('down');
-      break;
+      if (Keyboard.playableKeys.hasOwnProperty(e.code)) noteOn(e.code)
+      if (e.code === 'KeyZ') changeOctave('down')
+      if (e.code === 'KeyX') changeOctave('up')
+      break
     case 'keyup':
-      if (playable_keys.hasOwnProperty(e.code)) noteOff(e.code);
-      break;
+      if (Keyboard.playableKeys.hasOwnProperty(e.code)) noteOff(e.code)
+      break
   }
 }
 
-document.getElementById('power').addEventListener('click', (e) => {
-  console.log(e.target);
-  let checkbox = e.target;
+document.getElementById('power').addEventListener('click', e => {
+  let checkbox = e.target
 
   switch (checkbox.checked) {
     case true:
-      CONTEXT.resume().then(() => console.log('Power On'));
-      break;
+      CONTEXT.resume().then(() => console.log('Power On'))
+      break
     case false:
-      CONTEXT.suspend().then(() => console.log('Power Off'));
-      break;
+      CONTEXT.suspend().then(() => console.log('Power Off'))
+      break
   }
-});
+})
